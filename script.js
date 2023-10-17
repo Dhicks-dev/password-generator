a725e47c1060e7a2e9a08b1ebbcb320e277d4dd6
Deontae Hicks <72946905+Dhicks-dev@users.noreply.github.com>
2023-10-16 22:41:49 -0500

    added functionality on script js

---
+++ script.js
@@ -0,0 +1,33 @@
+// Assignment code here
+function generatePassword(length) {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    let password = '';
+    
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+    
+    return password;
+  }
+  
+  const length = parseInt(prompt('Enter the length of the password:'));
+  const password = generatePassword(length);
+  console.log('Generated password:', password);
+  
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
